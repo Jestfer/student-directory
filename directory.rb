8999c0f7ecdecded4550284e0de8290e8c36e943
Josué <jestfer6@gmail.com>
2018-01-15 18:04:24 +0000

    exercise 8.4

--- directory.rb
+++ directory.rb
@@ -23,8 +23,11 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].size < 12
+  i = 0
+  
+  while i < students.length
+    puts "#{students[i][:name]} (#{students[i][:cohort]} cohort)"
+    i += 1
   end
 end
 
