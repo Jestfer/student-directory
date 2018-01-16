31a64506e8be1cd2d3e2fe3cf001b3fe4195af6f
Josué <jestfer6@gmail.com>
2018-01-16 00:10:34 +0000

    typos fixed and exercises 8.11 - 8.12

--- typos.rb
+++ typos.rb
@@ -1,48 +1,43 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
+
   # create an empty array
   students = []
+
   # get the first name
   name = gets.chomp
+
   # while the name is not empty, repeat this code
   while !name.empty? do
-    # ask for cohort
-    puts "Which cohort do you belong to?"
-    cohort = gets.chomp
-    cohort == "" ? cohort = "Mmm... must be from the Heroes Academy" : cohort = cohort.to_sym
     # add the student hash to the array
-    students << {name: name, cohort: cohort}
+    students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
   end
+
   # return the array of students
-  p students
   students
 end
 
 def print_header
-  puts "The students of Villains Academy"
+  puts "The students of my cohort at Makers Academy"
   puts "-------------"
 end
 
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)" if students.length > 0
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer(students)
-  if students.count == 1
-    puts "Overall, we have #{students.count} great student"
-  else
-    puts "Overall, we have #{students.count} great students"
-  end
+  puts "Overall, we have #{students.count} great students"
 end
 
+
 students = input_students
-# nothing happens until we call the methods
 print_header
 print(students)
 print_footer(students)
