0baffff716f59aae4e634ce3d933ab0087f6600d
Josué <jestfer6@gmail.com>
2018-01-16 00:30:57 +0000

    interactive menu added

--- directory.rb
+++ directory.rb
@@ -18,7 +18,6 @@ def input_students
     name = gets.chomp
   end
   # return the array of students
-  p students
   students
 end
 
@@ -41,8 +40,31 @@ def print_footer(students)
   end
 end
 
-students = input_students
+def interactive_menu
+  students = []
+
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+
+    selection = gets.chomp
+
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+
 # nothing happens until we call the methods
-print_header
-print(students)
-print_footer(students)
+interactive_menu
